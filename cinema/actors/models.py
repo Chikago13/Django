61b7d565e5d6358168f28d69cdeff7ecda6fabe0
Chikago13 <sivko2201@gmail.com>
2023-10-07 19:10:31 +0300

    create new migrate

--- cinema/actors/models.py
+++ cinema/actors/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
 
-
 class Director(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя')
-    year = models.IntegerField(verbose_name='Возраст')
+    data = models.DateTimeField(verbose_name='Возраст')
     country = models.CharField(max_length=100, verbose_name='Страна')
-    number_films_made = models.IntegerField(verbose_name='Количество снятых фильмов')
-    movie_category = models.CharField(max_length=250, verbose_name='Категория фильма')
 
     def __str__(self):
         return f'id-{self.id}, {self.name}'
@@ -15,10 +12,10 @@ class Director(models.Model):
 
 class Actor(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя')
-    year = models.IntegerField(verbose_name='Возраст')
+    data = models.DateTimeField(verbose_name='Возраст')
     country = models.CharField(max_length=100, verbose_name='Страна')
-    number_films_made = models.IntegerField(verbose_name='Количество снятых фильмов')
     number_main_roles = models.IntegerField(verbose_name='Количество главных ролей')
 
     def __str__(self):
         return f'id-{self.id}, {self.name}'
+
--- cinema/movie/models.py
+++ cinema/movie/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from actors.models import Director, Actor
 
 
-
 class Film(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
     rating = models.FloatField(verbose_name='Рейтинг')
     duration = models.IntegerField(verbose_name='Продолжительность')
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
     actors = models.ManyToManyField(Actor)
 
     def __str__(self):
